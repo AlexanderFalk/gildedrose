1ecab96f3b097443356785cb48d295eb469c7f78
Alexander Falk <alexanderfalk@Alexanders-MacBook-Pro.local>
2017-08-08 21:45:38 +0200

    Uden fejl

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -20,7 +20,7 @@ public class GildedRoseTest {
         Item[] items = new Item[] {new Item("Sulfuras, Hand of Ragnaros", 0, 80)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(81, items[0].quality);
+        assertEquals(80, items[0].quality);
     }
 
 }
