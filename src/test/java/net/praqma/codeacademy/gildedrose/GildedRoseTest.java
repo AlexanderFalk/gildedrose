a682a06253c5e80b87c29835fbdb4563f75a7c80
Alexander Falk <alexanderfalk@Alexanders-MacBook-Pro.local>
2017-08-08 13:21:57 +0200

    Hey

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -11,6 +11,7 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
+        // Test
         assertEquals("fixme", app.items[0].name);
     }
 
